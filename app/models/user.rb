66660ed9c33c4d8254a988a7a0193ca8692d738d
dungbc-0212 <bui.cong.dung@sun-asterisk.com>
2023-09-11 16:59:45 +0700

    Chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 50},
@@ -13,5 +15,25 @@ format: {with: VALID_EMAIL_REGEX}
              BCrypt::Engine.cost
            end
     BCrypt::Password.create string, cost:
+  end
+
+  class << self
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_column :remember_digest, nil
   end
 end
