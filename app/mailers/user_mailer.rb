ec37ef400caf512a4b154b74b539dcea39990fa3
dungbc-0212 <bui.cong.dung@sun-asterisk.com>
2023-09-15 15:59:19 +0700

    Chapter 12

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,12 @@ class UserMailer < ApplicationMailer
   #
   def account_activation user
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: t("mail.mail_title")
+  end
+
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t("password_reset.mail_title")
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -14,9 +19,4 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.password_reset.subject
   #
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
-  end
 end
